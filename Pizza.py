6d564f539208228e0c8d6d96507a44282a1c2a33
janxgodlewski <63665194+janxgodlewski@users.noreply.github.com>
2021-11-18 14:54:05 +0100

    Update Pizza.py

--- Pizza.py
+++ Pizza.py
@@ -3,93 +3,69 @@
 
 # In[1]:
 
-
 #matplotlib
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'notebook')
 
-#do animacji i nałożenia zdjęć
+#libs to create animation and add images to pie chart
 from matplotlib.patches import PathPatch, Circle, Wedge, Polygon
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from matplotlib.animation import FuncAnimation
 
-#wyświetlenie wideo w HTML
+#libs to create video from animation
 from IPython.display import HTML
 from matplotlib import rc
 
 fig, ax = plt.subplots()
 
-#tytuł subplota
+#subplot title
 fig.suptitle('Pizzeria Pajton', fontsize=14, fontweight='bold')
 
-#dane do wykresu
+#chart data
 totals=[2,5,3]
 labels = ["Margherita [20%]", "Hawajska [50%]", "Capriciosa [30%]"]
 files = ["Margherita.png", "Hawajska.png", "Capriciosa.png"]
 
 
-#klatka animacji 
+#animation frame
 def animate(i):
-    #na początku każdej klatki animacji należy wyczyścić plota
+    #at the beginning of every frame axes needs to be cleared
     ax.clear()
     ax.set_xlabel('% Udział sprzedaży poszczególnych pizz w listopadzie', fontsize=10, fontweight='bold')
    
-    #odległość ćwiartek wykresu od środka
+    #distance of every wedge from center of the chart
     distance = i * 0.004
     explodes = [distance,distance,distance]
 
-    #rysowanie wykresu
+    #draw chart
     wedges, texts = ax.pie(totals, startangle=18, labels=labels,  explode = explodes, wedgeprops = { 'linewidth': 3, "edgecolor" :"papayawhip", "fill":False,  })
     
-    #nałożenie zdjęcia na każdą ćwiartkę wykresu
+    #display image on every wedge 
     for i in range(3):
         img_to_pie("C:\data\{}".format(files[i]), wedges[i], ax)
 
     return wedges
 
-#funkcja do nałożenia zdjęcia na ćwiartkę
+#function for displaying image on wedge
 def img_to_pie(file, wedge, ax):
-    #przybliżenie i pozycja zdjęcia w ćwiartce
+    #zoom and position of image in wedge
     zoom = 1
     position=(wedge.center[0], wedge.center[1])
     
-    #wczytujemy zdjęcie
+    #read image
     image = plt.imread(file, format='png')
     
-    #dopasowujemy zdjęcie do ćwiartki wykresu
+    #adjust image to chart wedge
     imagebox = OffsetImage(image, zoom, clip_path=wedge)
     
-    #stworzenie adnotacji i dodanie jej 
+    #create annotation and add it to axes
     ab = AnnotationBbox(imagebox, position, xycoords='data', pad=0, frameon=False)
     ax.add_artist(ab)
 
-#40 klatek animacji odtwarzanych co 50 ms. Parametr sprawia, że rysujemy tylko ten fragment wykresu, który się zmienił.
+#75 animation frames playing every 50 ms. Parameter blit ensures that we draw only these parts of chart which changed.
 anim = FuncAnimation(fig, animate, frames=75, interval=50, blit=True)
 
 rc('animation', html='html5')
 anim
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
